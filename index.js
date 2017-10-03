6e47ddc6c9ae9555bbce23e09a08aa33bdfe42df
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-03 18:40:06 +0530

    updated bot to api.ai

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
+const apiaiApp = require('apiai')(610c50319964449e8462dee0a274e3bb);
+
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -39,25 +41,59 @@ app.post('/webhook/', function (req, res) {
 
 var token = "EAABslGNoL6QBANvp5xlRviWBBkaiV0rdgHuxfiUU0Pf3LZCZAJF3VulksBaSuHwSVUEPcpYdyza1b7JBpUNwqY0ePJUgTB15YOzOe0pfulu2UaNoMIqpsATFm0slRZAObb4gCA4mFbn1rYVWqDZA2l5ReCEOzZAXWGiacu8gZBZCQZDZD"
 
+// function sendTextMessage(sender, text) {
+//     messageData = {
+//         text:text
+//     }
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token:token},
+//         method: 'POST',
+//         json: {
+//             recipient: {id:sender},
+//             message: messageData,
+//         }
+//     }, function(error, response, body) {
+//         if (error) {
+//             console.log('Error sending messages: ', error)
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error)
+//         }
+//     })
+// }
+
+//api.ai
+
 function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+
+    let apiai = apiaiApp.textRequest(text, {
+       sessionId: 'tabby_cat' // use any arbitrary id
+    });
+    
+    apiai.on('response', (response) => {
+     // Got a response from api.ai. Let's POST to Facebook Messenger
+     let aiText = response.result.fulfillment.speech;
+
+     request({
+       url: 'https://graph.facebook.com/v2.6/me/messages',
+       qs: {access_token: token},
+       method: 'POST',
+       json: {
+         recipient: {id: sender},
+         message: {text: aiText}
+       }
+     }, (error, response) => {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+    });
+    });
+
+    apiai.end();
+
+   
 }
 // Spin up the server
 app.listen(app.get('port'), function() {
