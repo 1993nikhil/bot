a2c992689d3fbadeacb2ac683ac9f245bbf59b6d
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-05 11:03:01 +0530

    architecture changed

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
+
+//var token = "EAABslGNoL6QBANvp5xlRviWBBkaiV0rdgHuxfiUU0Pf3LZCZAJF3VulksBaSuHwSVUEPcpYdyza1b7JBpUNwqY0ePJUgTB15YOzOe0pfulu2UaNoMIqpsATFm0slRZAObb4gCA4mFbn1rYVWqDZA2l5ReCEOzZAXWGiacu8gZBZCQZDZD"
+
+var token = {
+	492841481078418: 'EAABslGNoL6QBANvp5xlRviWBBkaiV0rdgHuxfiUU0Pf3LZCZAJF3VulksBaSuHwSVUEPcpYdyza1b7JBpUNwqY0ePJUgTB15YOzOe0pfulu2UaNoMIqpsATFm0slRZAObb4gCA4mFbn1rYVWqDZA2l5ReCEOzZAXWGiacu8gZBZCQZDZD',
+	121736051845888: 'EAABslGNoL6QBAHde5o5OraLFiCOk8L9ydxIJEDaFbZAJvjZCKww6dkihY9YbfU6uB7m45ZBLOwuvMBZCDZBeY7Bzr8rla94tjpXY1iKeSPxT15qzfHmm0VhkJZBq55ZCTQDsraTBGZCUTHk7k6FOskjPrbZBrAgroqhlvWEDmxzgkWAZDZD'
+}
+
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
@@ -32,74 +40,109 @@ app.post('/webhook/', function (req, res) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
         pageId = event.recipient.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, pageId, "Text received, echo: " + text.substring(0, 200))
-        }
+        if (event.message) {
+        	receivedMessage(event);
+		}
     }
     res.sendStatus(200)
 })
 
-//var token = "EAABslGNoL6QBANvp5xlRviWBBkaiV0rdgHuxfiUU0Pf3LZCZAJF3VulksBaSuHwSVUEPcpYdyza1b7JBpUNwqY0ePJUgTB15YOzOe0pfulu2UaNoMIqpsATFm0slRZAObb4gCA4mFbn1rYVWqDZA2l5ReCEOzZAXWGiacu8gZBZCQZDZD"
+function receivedMessage(event) {
+	var senderID = event.sender.id;
+    var pageId = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-var token = {
-	492841481078418: 'EAABslGNoL6QBANvp5xlRviWBBkaiV0rdgHuxfiUU0Pf3LZCZAJF3VulksBaSuHwSVUEPcpYdyza1b7JBpUNwqY0ePJUgTB15YOzOe0pfulu2UaNoMIqpsATFm0slRZAObb4gCA4mFbn1rYVWqDZA2l5ReCEOzZAXWGiacu8gZBZCQZDZD',
-	121736051845888: 'EAABslGNoL6QBAHde5o5OraLFiCOk8L9ydxIJEDaFbZAJvjZCKww6dkihY9YbfU6uB7m45ZBLOwuvMBZCDZBeY7Bzr8rla94tjpXY1iKeSPxT15qzfHmm0VhkJZBq55ZCTQDsraTBGZCUTHk7k6FOskjPrbZBrAgroqhlvWEDmxzgkWAZDZD'
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+    
+     if (messageText) {
+      messageText= messageText.toLowerCase();
+      switch (messageText) {
+        case 'generic':
+        sendTextMessage(senderID, pageId, "Thank You for your Response, have a nice Day");
+        break;
+        case 'hi':
+        getUserName(senderID, pageId);
+        break;
+        case 'hello':
+        getUserName(senderID, pageId);
+        break;
+        
+            
+        default:
+        sendTextMessage(senderID, pageId, "Thank You for your Response, have a nice Day",1);
+      }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, pageId, "Message with attachment received");
+    }  
+
 }
-// function sendTextMessage(sender, text) {
-//     messageData = {
-//         text:text
-//     }
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/messages',
-//         qs: {access_token:token},
-//         method: 'POST',
-//         json: {
-//             recipient: {id:sender},
-//             message: messageData,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
-
-//api.ai
 
 function sendTextMessage(sender, pageId, text) {
+	var messageData = {
+		 recipient: {
+           id: sender
+        },
+        message: {
+          text: messageText
+        }
+    };
 
-    let apiai = apiaiApp.textRequest(text, {
-       sessionId: 'tabby_cat' // use any arbitrary id
-    });
-    
-    apiai.on('response', (response) => {
-     // Got a response from api.ai. Let's POST to Facebook Messenger
-     let aiText = response.result.fulfillment.speech;
-
-     request({
-       url: 'https://graph.facebook.com/v2.6/me/messages',
-       qs: {access_token: token[pageId]},
-       method: 'POST',
-       json: {
-         recipient: {id: sender},
-         message: {text: aiText}
-       }
-     }, (error, response) => {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-    });
-    });
-
-    apiai.end();
-
-   
+    callSendAPI(messageData, pageId);
 }
+
+function callSendAPI(messageData,pageId) {
+
+ 
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token[pageId]},
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      //console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+//getUserName
+function getUserName(userId,pageId) {
+  var getInfoUserAPI=' https://graph.facebook.com/v2.6/'+userId+'?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token='+token[pageId];
+
+ request({
+    uri: getInfoUserAPI,
+    method: 'GET',   
+
+  }, function (error, response, body) {
+ 
+    if (!error && response.statusCode == 200) {
+     var jsonData = JSON.parse(body);
+     var newMessage = jsonData.first_name+" "+jsonData.last_name;
+     callSendAPI(newMessage, pageId)
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  }); 
+
+
+}
+
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
