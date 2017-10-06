7d73687eed5aaff4b7841692ab06fcfc08407159
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-06 15:26:51 +0530

    callSendAPI issue

--- services/greetingService.js
+++ services/greetingService.js
@@ -83,7 +83,7 @@ function startConversationQ(userId, messageText){
         } 
       };
 
-  callSendAPI(messageData);
+  fbCtrl.callSendAPI(messageData);
 }
 
 function startConversation(userId, messageText){
@@ -146,31 +146,31 @@ function startConversation(userId, messageText){
     };
 
 
-  callSendAPI(messageData);
+  fbCtrl.callSendAPI(messageData);
 }
 
 
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: messageData
+// function callSendAPI(messageData) {
+//   request({
+//     uri: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: {access_token: token},
+//     method: 'POST',
+//     json: messageData
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+//   }, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var recipientId = body.recipient_id;
+//       var messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      //console.error(response);
-      console.error(error);
-    }
-  });  
-}
+//       console.log("Successfully sent generic message with id %s to recipient %s", 
+//         messageId, recipientId);
+//     } else {
+//       console.error("Unable to send message.");
+//       //console.error(response);
+//       console.error(error);
+//     }
+//   });  
+// }
 
 module.exports = {
    getUserName:getUserName,
