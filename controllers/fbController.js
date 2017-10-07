275b9d1115e6d29228563a3bf562d9fa75ceece8
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-08 02:05:06 +0530

    question flow

--- controllers/fbController.js
+++ controllers/fbController.js
@@ -43,7 +43,7 @@ function receivedMessage(event) {
      } else if (messageAttachments) {
         sendTextMessage(senderID, "Message with attachment received");
      } 
-
+     console.log(index);
 
     // if(!saveUserOffset){
     // 	saveUser(senderID);
