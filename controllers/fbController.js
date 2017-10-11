1af9a8f4298f6aa60cdd0a236754cbcf80985c80
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-11 16:28:18 +0530

    sync calling

--- controllers/fbController.js
+++ controllers/fbController.js
@@ -263,8 +263,7 @@ function nextQuestion(questionIndex,payload,recipientId){
           text: "OTP is send to your register no. please provide that"
         }      
       }  
-
-      fbService.getPolicyData(recipientId,indexArray[1]).then(function(resp){
+      fbService.saveResponse(recipientId,newQuestionIndex,payload).then(fbService.getPolicyData(recipientId,indexArray[1]).then(function(resp){
         if(util.validatePolicy(resp)){
           generateOtp(recipientId);
           callSendAPI(messageData);
@@ -273,7 +272,8 @@ function nextQuestion(questionIndex,payload,recipientId){
           sendTextMessage(recipientId,'Policy Details not matched please type Hi/Hello to start your journey again');
           fbService.updateQuestionIndex(recipientId,"0-null-null");
         }
-      });
+      })
+      );
      
     }else{
      var messageData ={
@@ -358,10 +358,9 @@ function nextDueData(recipientId,category){
    var nextDueMsg = utilMsg.messages.nextDueMessage;
    var messageData = nextDueMsg.replace("#policyid#",resp);
 
-   sendTextMessage(recipientId,messageData);
+   sendTextMessage(recipientId,messageData).then(nextQuestion("5-NP-DATA","yes",recipientId));
   });
-  nextQuestion("5-NP-DATA","yes",recipientId);
-
+  
 }
 
 function generateOtp(recipientId){
