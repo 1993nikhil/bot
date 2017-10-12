85089d90d6aeed6c3334d9a5a0839fb42b069b54
nikhil <nikhil.kumar@geminisolutions.in>
2017-10-12 15:30:47 +0530

    \n

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,7 @@
 module.exports = {
 	 messages:{
 		"greeting":"Hi #userName#. I am RIA , welcome to DHFL Bot. I can help you with the following services",
-		"nextDueMessage":"Please check Next Due Date regarding #policyid# policy No. :- \n Premium Due Date:- 25-12-2017  Premium Amount:- 4566",
+		"nextDueMessage":"Please check Next Due Date regarding #policyid# \t\t\tpolicy No. :- \n \t\t\tPremium Due Date:- 25-12-2017 \n \t\t\t Premium Amount:- 4566",
 		"thankyouMessage":"Thanks for using a service. Have a nice day",
 		"queryMessage":"For any query & clarification please call our toll free number 1800-102-7070 or email - contactus@dhflpramerica.com",
 	}
