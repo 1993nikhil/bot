2a81459185d7644f03bc5e64deeba7c64862e907
nikhil <nikhil.kumar@geminisolutions.in>
2017-11-23 13:08:09 +0530

    validate policy test

--- utils/utils.js
+++ utils/utils.js
@@ -16,13 +16,16 @@ module.exports = {
 			policyService.getPolicyDetail(response.policyNo).then(function(resp){
 				if(resp){
 					if(resp.policyNo===response.policyNo && resp.DOB===response.DOB){
+						console.log("detail matched");
 						return resp;
 					}
 					else{
+						console.log("detail not matched in if");
 						return null
 					}
 				}
 				else{
+					console.log("detail not matched outside if");
 					return null
 				}
 			});
