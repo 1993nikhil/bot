ba064cbd809e14d2b1faf4c5435797abaf253fa6
nikhil <nikhil.kumar@geminisolutions.in>
2017-11-28 15:58:52 +0530

    Messages changed

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,10 @@
 module.exports = {
 	 messages:{
 		"greeting":"Dear #userName#, #greet#. Welcome to DHFL Pramerica Life insurance. I can help you with the following services \nCustomer Services option helps you fulfill your policy servicing request. e.g.- Policy renewal date, fund value etc with the help of RIA Bot.",
-		"nextDueMessage":"Your Next Premium Due Date for Policy No. #policyid# is:- \n \t\tPremium Due Date :- 25-12-2017 \n \t\tPremium Amount :- 4566",
-		"fundValueMessage":"Your fund value for Policy No. #policyid# is:-\n \t\t\tNAV\n \t\tFund value:4556(unit*NAV)\n \t\tFund Name:Balance Fund & units:56\n \t\tTotal Fund Value:45600(unit:NAV)",
-		"totalAmtMessage":"Your Total amount paid till date for Policy No. #policyid# is:- Total Amount Paid:-4567",
-		"policyStatusMessage":"Your Policy Status for Policy No. #policyid# is:- Premium Paying",
+		"nextDueMessage":"Your Next Premium Due Date for Policy No. #policyid# is :- \n \t\tPremium Due Date :- 25-12-2017 \n \t\tPremium Amount :- 4566",
+		"fundValueMessage":"Your fund value for Policy No. #policyid# is :-\n \t\t\tNAV\n \t\tFund value : 4556(unit*NAV)\n \t\tFund Name : Balance Fund & units : 56\n \t\tTotal Fund Value : 45600(unit:NAV)",
+		"totalAmtMessage":"Your Total amount paid till date for Policy No. #policyid# is :- Total Amount Paid :- 4567",
+		"policyStatusMessage":"Your Policy Status for Policy No. #policyid# is :- Premium Paying",
 		"payPremiumMessage":"You will be redirected to our DHFL Pramercia Life insurance site for pay renewal payment",
 		"thankyouMessage":"Thanks for using a service. Have a nice day",
 		"queryMessage":"For any query & clarification please call our toll free number 1800-102-7070 or email - contactus@dhflpramerica.com",
