cfa4d725c443a5f1fac4ce917f33582b45de4092
nikhil <nikhil.kumar@geminisolutions.in>
2017-11-30 11:08:05 +0530

    fund value msg

--- utils/messages.js
+++ utils/messages.js
@@ -2,7 +2,8 @@ module.exports = {
 	 messages:{
 		"greeting":"Dear #userName#, #greet#. Welcome to DHFL Pramerica Life insurance. I can help you with the following services \nCustomer Services option helps you fulfill your policy servicing request. e.g.- Policy renewal date, fund value etc with the help of RIA Bot.",
 		"nextDueMessage":"Your Next Premium Due Date for Policy No. #policyid# is :- \n \t\tPremium Due Date :- 25-12-2017 \n \t\tPremium Amount :- 4566",
-		"fundValueMessage":"Your fund value for Policy No. #policyid# is :-\n \t\t\tNAV\n \t\tFund value : 4556(unit*NAV)\n \t\tFund Name : Balance Fund & units : 56\n \t\tTotal Fund Value : 45600(unit:NAV)",
+		//"fundValueMessage":"Your fund value for Policy No. #policyid# is :-\n \t\t\tNAV\n \t\tFund value : 4556(unit*NAV)\n \t\tFund Name : Balance Fund & units : 56\n \t\tTotal Fund Value : 45600(unit:NAV)",
+		"fundValueMessage":"Your Fund value for Policy No. #policyid# is :-\nFund Name :- Balanced Fund\nNet Asset Value :- 18.33\nNumber of Units :- 16\nFund Value :- 14000\n\nFund Name :- Growth Fund\nNet Asset Value :- 18.33\nNumber of Units :- 16\nFund Value :- 14000\n\nFund Name :- Equity Fund\nNet Asset Value :- 18.33\nNumber of Units :- 16\nFund Value :- 14000\n\nTotal Fund Value :- 49999",
 		"totalAmtMessage":"Your Total amount paid till date for Policy No. #policyid# is :- Total Amount Paid :- 4567",
 		"policyStatusMessage":"Your Policy Status for Policy No. #policyid# is :- Premium Paying",
 		"payPremiumMessage":"You will be redirected to our DHFL Pramercia Life insurance site for pay renewal payment",
